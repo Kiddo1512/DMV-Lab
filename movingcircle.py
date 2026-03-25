ed5b079622e96a34423d3a76df384420769ecb9d
Kiddo1512 <blackriders.1409@gmail.com>
2026-03-25 14:54:21 +0530

    Add files via upload

--- movingcircle.py
+++ movingcircle.py
@@ -1,21 +1,42 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+
+x_limit = int(input("Enter X-axis limit (e.g., 10): "))
+y_limit = int(input("Enter Y-axis limit (e.g., 10): "))
+circle_radius = float(input("Enter circle radius (e.g., 0.5): "))
 
 fig, ax = plt.subplots()
 
-ax.set_xlim(0, 10)
-ax.set_ylim(0, 10)
+ax.set_xlim(0, x_limit)
+ax.set_ylim(0, y_limit)
 
-circle = plt.Circle((0, 5), 0.5)
+x, y = x_limit // 2, y_limit // 2
+
+circle = plt.Circle((x, y), circle_radius)
 ax.add_patch(circle)
 
-def update(frame):
-    circle.center = (frame, 5)
-    return circle,
+step = 0.5
 
-ani = animation.FuncAnimation(fig, update, frames=range(11), interval=100)
+def on_key(event):
+    global x, y
+
+    if event.key == 'left':
+        x -= step
+    elif event.key == 'right':
+        x += step
+    elif event.key == 'up':
+        y += step
+    elif event.key == 'down':
+        y -= step
+
+    x = max(circle_radius, min(x, x_limit - circle_radius))
+    y = max(circle_radius, min(y, y_limit - circle_radius))
+
+    circle.center = (x, y)
+    fig.canvas.draw()
+
+fig.canvas.mpl_connect('key_press_event', on_key)
 
 plt.gca().set_aspect('equal', adjustable='box')
-plt.title("Moving Circle Animation")
+plt.title("Use Arrow Keys to Move Circle")
 
 plt.show()
